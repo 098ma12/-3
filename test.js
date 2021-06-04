de89b9d07c786388789daa2de47f3236c052fc94
EC2 Default User <ec2-user@ip-172-31-2-200.us-east-2.compute.internal>
2021-06-04 04:38:53 +0000

    課題

--- test.js
+++ test.js
@@ -1,38 +1,97 @@
-let myTotal = 0;	// 現在の合計値
-let myInput = "";	// 現在入力している値
-let myCalc = "+";	// 合計と入力値の演算子
-let myFlg = 1;	// １回前に入力したもの 0:数字 1:演算子
- 	
-function myValue(myData){	// 1～9または小数点ボタンを押した
-myFlg = 0;	// １回前に入力したものは数値
-myInput += myData;	// 現在入力している値に追加
-document.myForm.myLine.value = myInput;	// 現在入力している値表示
-}	
- 	
-function myCalculate(myData){	// 演算ボタンを押した
-if (myFlg==0){	// １回前に入力したものは数値か？
-myFlg = 1;	// １回前に入力したものは演算子
-myWork = myTotal + myCalc + myInput;	// 一連の計算式を作る
-myTotal = eval(myWork);	// 計算式を計算させる
-myInput = "";	// 現在入力している値をクリア
-document.myForm.myLine.value = myTotal;	// 合計を表示
-}	
-if (myData == "="){	// 演算ボタンは[＝]か？
-myTotal = 0;	// 合計をクリア
-myCalc = "+";	// 演算子を[+]とする
-}else{	// 演算ボタンは[＝]以外である
-myCalc = myData;	// 演算子を退避させておく
-}	
-}	
- 	
-function myC(){	// クリアボタン[C]を押した
-myTotal = 0;	// 合計クリア
-myCalc = "+";	// 演算子クリア
-myInput = "";	// 現在入力している値をクリア
-document.myForm.myLine.value = myTotal;	// つまり、０を表示
-}	
-function zero(mydate){//0を押した時
-if myFlg=zero//前の数字が0
-document.myForm.myLine.value = myTotal;	// ０を表示
-}	
+$(function(){
+  console.log('exit');
+ var total = 0; //現在の式
+ var operator = ''; //演算子
+ var currentValue = ''; //現在の値
+ var flag = 0; //ボタンが押されたあとに効かなくする※flag(フラグ)を立てる。最初は0を代入。
+
+
+
+
+$('[id=number]').on('click', function() {
+   let action = $(this).text();
+   console.log(action);
+  if (currentValue.length <= 8) {
+    flag = 0;
+    currentValue += action;
+    $('#number-text').text(currentValue);
+    console.log(currentValue); //確認用
+  }
+});
+
+
+$('#zero').on('click', function() {
+  let action = $(this).text();
+　 if (currentValue > 0) {
+    flag = 0;
+    currentValue += action;
+    $('#number-text').text(currentValue);
+  }else {
+    return;
+  }
+});
+
+
+$('#clear-btn').on('click', function() {
+  operator = '';
+  total = '';
+  currentValue = '';
+  flag = 0;
+  $('#number-text').text('0');
+  console.log(0);
+});
+
+
+$('[id=calc]').on('click', function() {
+   let action = $(this).text();
+　 if (currentValue === '') {
+    return; //最初に演算子を押せなくする
+  }else if (flag === 0 && action !== "=") {
+    flag = 1;
+    let formula = total + operator + currentValue;
+    total = eval(formula);
+    
+    operator = action;
+    currentValue = '';
+    
+    $('#number-text').text(total);
+    console.log(total);
+  } else if (flag === 1 && action === "=") {
+    let formula = total + operator + currentValue;
+    total = eval(formula);
+    
+
+
+    currentValue = "";
+    $('#number-text').text(total);
+  } else if (action === "=") {
+    flag = 1;
+
+
+    var formula = total + operator + currentValue;
+    total = eval(formula);
+    currentValue = "";
+    operator = action;
+    $('#number-text').text(total);
+  } else {
+    operator = action;
+  }
+});
+
+
+})
+
+
+$('#point').on('click', function() {
+    let action = $(this).text();
+    if (currentValue === '') {
+      return;
+    } else {
+     let f = currentValue + '.';
+    $('#number-text').text(f); 
+    }
+});
+
+
+
 
